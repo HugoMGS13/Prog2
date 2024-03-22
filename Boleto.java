9984a72da8546900c6150659e50a426d568377ad
HugoMGS13 <127580302+HugoMGS13@users.noreply.github.com>
2024-03-21 21:35:19 -0300

    Add files via upload

---
+++ Boleto.java
@@ -0,0 +1,7 @@
+public class Boleto {
+    public void emitirboleto(){
+        System.out.println("Boleto impresso");
+    }
+    public void cancelarboleto(){}
+    public void receberboleto(){}
+}
