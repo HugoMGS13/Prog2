26814ef87cc5c0886ded537c9b0f836a5713ae2d
HugoMGS13 <127580302+HugoMGS13@users.noreply.github.com>
2024-03-21 21:34:59 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,16 @@
+public class App {
+    public void emitirboleto(){
+        Boleto variavelBoleto = new Boleto();
+        variavelBoleto.emitirboleto();
+    }
+}
+
+
+/* Entidades que dividem responsabilidades do código entre si (Respomsabilidade é algo subjetivo)
+ * As entidades são chamadas de CLASSES
+ * Geralmente as responsabilidades são diferenciadas por ações
+ * Cada classe é um arquivo .java diferente
+ * Os nomes das classes têm que armazenar o tema e as ações devem estar nos nomes dos métodos
+ * Ação = Método, Abstração/Tema/Tópico = Classes , Objeto = Indivíduo específico de uma classe (Um objeto específico de uma classe)
+ * Objetos são indivíduos, instâncias de classe, as quais armazenam os objetos
+*/
