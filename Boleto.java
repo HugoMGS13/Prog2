6ca3409fff0afe1fdd5f37ae8c49ab8dca0c00d0
HugoMGS13 <127580302+HugoMGS13@users.noreply.github.com>
2024-04-02 21:26:20 -0300

    Update Boleto.java

--- Boleto.java
+++ Boleto.java
@@ -1,7 +1,15 @@
 public class Boleto {
-    public void emitirboleto(){
-        System.out.println("Boleto impresso");
+
+    double valor;
+    String vencimento;
+    String codigoDeBarra;
+
+    public void emitirboleto(Boleto b) /*Recebendo uma variável do tipo Boleto*/{
+        System.out.println("Boleto impresso" + valor + vencimento + codigoDeBarra);
+        impressora imp = new impressora();
+        imp.imprimir(b) /*Enviando a variável b para o método imprimir da classe impressora */;
     }
+
     public void cancelarboleto(){}
     public void receberboleto(){}
 }
