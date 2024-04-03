b4aeec6402a915357ccf8d9159216478b24cec07
HugoMGS13 <127580302+HugoMGS13@users.noreply.github.com>
2024-04-02 21:26:34 -0300

    Update impressora.java

--- impressora.java
+++ impressora.java
@@ -1,3 +1,7 @@
 public class impressora {
+    public void imprimir(Boleto b) /*Recebendo uma variável b do tipo Boleto*/{
+        b.valor = 100; //Não pode fazer isso
+        System.out.println("Imprimir");
+    }
     
 }
