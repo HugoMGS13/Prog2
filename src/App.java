3de5ca2741c1090afc0788841365873b6f7b9277
Hugo <127580302+HugoMGS13@users.noreply.github.com>
2024-09-04 19:39:03 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Cliente cliente = new Cliente("Hugo", 03223);
-        ContaBancaria conta = new ContaBancaria(cliente);
-        System.out.println(cliente.getNome());
+        Cliente Hugo = new Cliente("Hugo", 03334567);
+        ContaBancaria contaBB = new ContaBancaria(Hugo, 22218898, 50000);
+        Emprestimo emprestimo = new Emprestimo(Hugo,22218898 , 50000, 4000);
+        emprestimo.verificarEmprestimo(emprestimo);
     }
 }
