e58b4e2e96d8b08bfc2a56c285a6c9309f843be0
Hugo <127580302+HugoMGS13@users.noreply.github.com>
2024-11-04 23:43:33 -0300

    Delete Ifood Uni directory

--- Ifood Uni/Bicicleta.java
+++
@@ -1,2 +0,0 @@
-public class Bicicleta implements Veiculo{
-}
--- Ifood Uni/Mercado.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class Mercado extends Estabelecimento {
-    public Mercado(String nome, ArrayList<Produto> Itens, double avalicao){
-        super(nome, Itens, avalicao);
-    }
-
-}
